622fbb7e7e2987548c81f7ba7371395a4bc20423
Oh...Yeah!!! <614988210@qq.com>
2024-05-23 23:26:13 +0800

    +1

--- jvm/src/main/java/AddOneTest.java
+++ jvm/src/main/java/AddOneTest.java
@@ -3,10 +3,16 @@ public class AddOneTest {
 
     public static void main(String[] args) {
 
+        /*
+        * 测试下方哪种方法的性能更高
+        *  第一种和第三种最高
+        *  第二种最低
+        * */
         int i = 0,j = 0,k = 0;
         i++;
         j = j + 1;
         k += 1;
+
     }
 
 }
